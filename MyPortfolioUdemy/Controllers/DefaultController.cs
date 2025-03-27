483c823f02d1954d9871a1424f334c5c6249a77a
Dogukan Babayigit <doukanb@proton.me>
2025-03-27 16:33:52 +0300

    Components #D2
    Completed The Components

---
+++ MyPortfolioUdemy/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioUdemy.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: DefaultController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MyPortfolioUdemy/ViewComponents/_FooterComponentPartial.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioUdemy.ViewComponents;
+
+public class _FooterComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}

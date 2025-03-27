a63cb41d7aee698992a9f55717882b5a4a6bbbb7
Dogukan Babayigit <doukanb@proton.me>
2025-03-27 16:51:47 +0300

    Database Works#D2

--- MyPortfolioUdemy/ViewComponents/_FeatureComponentPartial.cs
+++ MyPortfolioUdemy/ViewComponents/_FeatureComponentPartial.cs
@@ -1,12 +1,20 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using MyPortfolioUdemy.DAL.Context;
 
 namespace MyPortfolioUdemy.ViewComponents;
 
 public class _FeatureComponentPartial : ViewComponent
 {
+    private readonly MyPortfolioContext _context;
+
+    public _FeatureComponentPartial(MyPortfolioContext context)
+    {
+        _context = context;
+    }
     public IViewComponentResult Invoke()
     {
-        return View();
+        var values = _context.Features.ToList();
+        return View(values);
     }
 }

a1b7556755e064a0fdb0afd04fc4f3f65b5b8158
Dogukan Babayigit <doukanb@proton.me>
2025-04-04 14:43:04 +0300

    #D4 Admin Panel
    Started building panel view and components

---
+++ MyPortfolioUdemy/Controllers/ExperinceController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioUdemy.Controllers
+{
+    public class ExperinceController : Controller
+    {
+        // GET: ExperinceController
+        public ActionResult ExperienceList()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MyPortfolioUdemy/Controllers/LayoutController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioUdemy.Controllers
+{
+    public class LayoutController : Controller
+    {
+        // GET: LayoutController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
